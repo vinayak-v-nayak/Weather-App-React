c8dc253319387cb456e0f8f800dfe7fa87badb6e
Vinayaka V Nayak <106983430+vinayak-v-nayak@users.noreply.github.com>
2024-09-27 21:42:49 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_KEY = '8a0b526caaa963c976fce0199bbbc489';
+const API_KEY = '';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const fetchCurrentWeather = async (city) => {
